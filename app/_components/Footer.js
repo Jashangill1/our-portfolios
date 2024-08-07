82d72c86b3b5f5a269b5d3f6928688bc81a96bfe
Ravneetd13 <ravneets2308@gmail.com>
2024-08-06 20:24:37 -0600

    Update Footer.js

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="w-full py-4 static bg-white bottom-0">
+    <footer className="w-full py-4 static bg-slate-500 bottom-0">
       <div className="flex justify-center">
-        <p className="text-gray-600">&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+        <p className="text-gray-600">&copy; 2024 My Portfolio. All rights reserved.</p>
       </div>
     </footer>
   )
