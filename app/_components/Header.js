8c03f37bdfd3d3f607afd3e37a15e3dc6eaf411d
Ravneetd13 <ravneets2308@gmail.com>
2024-08-06 20:25:14 -0600

    Update Header.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -4,16 +4,16 @@ const Header = () => {
   return (
     <header className="w-full py-4 bg-white shadow-md top-0 fixed">
       <nav className="flex justify-center gap-4">
-        <Link href="/" className="text-blue-500 hover:text-blue-700">
+        <Link href="/" className="text-green-500 hover:text-green-700">
           Home
         </Link>
-        <Link href="/about" className="text-blue-500 hover:text-blue-700">
+        <Link href="/about" className="text-green-500 hover:text-green-700">
           About
         </Link>
-        <Link href="/projects" className="text-blue-500 hover:text-blue-700">
+        <Link href="/projects" className="text-green-500 hover:text-green-700">
           Projects
         </Link>
-        <Link href="/contact" className="text-blue-500 hover:text-blue-700">
+        <Link href="/contact" className="text-green-500 hover:text-green-700">
           Contact
         </Link>
       </nav>
